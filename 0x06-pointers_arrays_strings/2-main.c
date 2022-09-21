b14f6b64f1294e11c785313182454d4c1926a995
Alafara1 <Ibrahimalafara@gmail.com>
2022-09-21 13:13:28 -0700

    .

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -1,41 +1,28 @@
 #include "alafara.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * _strncpy - Copies a string
  *
- * Return: Always 0.
+ * @dest: Buffer pointed to be copied into
+ * @src: string to be copied from
+ * @n: limit of string to copy from src
+ *
+ * Return: Pointer to the resulting string dest
+ *
  */
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
 {
-  char s1[98];
-  char *p;
-  int i;
+	int i;
 
-  for (i = 0; i < 98 - 1; i++)
-    {
-      s1[i] = '*';
-    }
-  s1[i] = '\0';
-  printf("%s\n", s1);
-  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-  printf("%s\n", s1);
-  printf("%s\n", p);
-  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-  printf("%s", s1);
-  printf("%s", p);
-  for (i = 0; i < 98; i++)
-    {
-      if (i % 10)
-        {
-	  printf(" ");
-        }
-      if (!(i % 10) && i)
-        {
-	  printf("\n");
-        }
-      printf("0x%02x", s1[i]);
-    }
-  printf("\n");
-  return (0);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
 }
