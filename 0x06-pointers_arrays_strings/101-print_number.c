f1ce48db7411752951584bd42570b0b0fcf4030b
Alafara1 <Ibrahimalafara@gmail.com>
2022-09-22 19:06:09 -0700

    .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,27 @@
 #include "alafara.h"
+
 /**
- * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
+ * print_number - Prints an integer
+ *
+ * @n: Integer value
+ *
+ * Return: void
  */
 void print_number(int n)
 {
+	unsigned int n1;
 
-	return (0);
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = n * -1;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
